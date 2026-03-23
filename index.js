c9891b382d1f18193ffa5b62b769faf8bce31ad3
LWP10 <john@legacywillsandprobate.com>
2026-03-23 12:58:51 +0000

    Update index.js

--- index.js
+++ index.js
@@ -81,8 +81,8 @@ wss.on("connection", (ws, req) => {
         turn_detection: {
           type: "server_vad",
           threshold: 0.5,
-          prefix_padding_ms: 300,
-          silence_duration_ms: 1600,
+          prefix_padding_ms: 400,
+          silence_duration_ms: 1700,
         },
 
         // === MAIN BEHAVIOUR PROMPT ===
